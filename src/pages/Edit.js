c399a449dfe09ecbb87fc5f94543100c6e17a20d
Parzival8971 <esh8971@gmail.com>
2022-09-23 22:39:01 +0900

    learnning react

---
+++ src/pages/Edit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { useSearchParams } from 'react-router-dom';
+
+const Edit = () => {
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const id = searchParams.get('id');
+  // console.log(id);
+  // const mode = searchParams.get('mode');
+  // console.log(mode);
+  return <div>Edit</div>;
+};
+
+export default Edit;
