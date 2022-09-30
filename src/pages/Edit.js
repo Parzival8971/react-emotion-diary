9b97375a62e8138f06ad1c02c1369a13c95c3f94
Parzival8971 <esh8971@gmail.com>
2022-09-30 18:05:15 +0900

    Minor fix

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { DiaryStateContext } from '../App';
-import DiaryEditor from '../componets/DiaryEditor';
+import DiaryEditor from '../components/DiaryEditor';
 
 const Edit = () => {
   const [originData, setOriginData] = useState();
@@ -9,14 +9,18 @@ const Edit = () => {
   const { id } = useParams();
 
   const diaryList = useContext(DiaryStateContext);
-  // console.log(diaryList);
+
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `감정 일기장 - ${id}번 일기 수정`;
+  }, []);
 
   useEffect(() => {
     if (diaryList.length >= 1) {
       const targetDiary = diaryList.find(
         (it) => parseInt(it.id) === parseInt(id)
       );
-      // console.log(targetDiary);
+
       if (targetDiary) {
         setOriginData(targetDiary);
       } else {
