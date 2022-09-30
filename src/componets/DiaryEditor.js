4e44c5df8ed2f7b1418d681782fa7ac27b48e906
Parzival8971 <esh8971@gmail.com>
2022-09-30 17:49:43 +0900

    상세페이지를 구현한다

--- src/componets/DiaryEditor.js
+++ src/componets/DiaryEditor.js
@@ -1,52 +1,13 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { DiaryDispatchContext } from '../App';
+
+import MyHeader from './MyHeader';
+import MyButton from './MyButton';
 import EmotionItem from './EmotionItem';
 
-import MyButton from './MyButton';
-import MyHeader from './MyHeader';
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: '완전 좋음',
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: '좋음',
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: '그럭저럭',
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: '나쁨',
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: '완전 나쁨',
-  },
-];
-
-const getStringDate = (date) => {
-  // return date.toISOString().slice(0, 10);
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-  if (month < 10) {
-    month = `0${month}`;
-  }
-  if (day < 10) {
-    day = `0${day}`;
-  }
-  return `${year}-${month}-${day}`;
-};
+import { getStringDate } from '../util/date';
+import { emotionList } from '../util/emotion';
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
